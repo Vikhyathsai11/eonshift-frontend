f652bd64edb20ffd175c27578bb7fff3da9b3196
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2023-12-12 02:27:37 +0530

    feat: Updated the title and favicon and added chart..

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,8 @@ import { useFirestoreCollectionData, useUser } from "reactfire";
 
 import { ModeToggle } from "~/shared/theme-toggle";
 
+import SampleChart from "~/app/dashboard/components/sample";
+
 import { Button } from "~/lib/components/ui/button";
 import { db } from "~/lib/firebase";
 
@@ -49,20 +51,7 @@ const Facilities = () => {
 };
 
 export default function HomePage() {
-  // const { status: facilitiesStatus, data: facilities = [] as Facility[] } =
-  //   useFirestoreCollectionData(query(collection(db, "facilities")));
-
-  // const { status, data } = useSigninCheck();
-
-  // const [user, loading, error] = useAuthState(auth, {
-  //   onUserChanged: (user: User | null): Promise<void> => {
-  //     console.log(user);
-  //     return Promise.resolve();
-  //   },
-  // });
-  //
-
-  const { data, status, error } = useUser();
+  const { data, status } = useUser();
 
   return (
     <main>
@@ -78,46 +67,8 @@ export default function HomePage() {
         <Facilities />
         <Button onClick={() => console.log("Sign out.")}>Sign out</Button>
         <ModeToggle />
+        <SampleChart />
       </div>
-      {/*  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">*/}
-      {/*    {facilities.map((facility) => (*/}
-      {/*      <div*/}
-      {/*        key={facility.id}*/}
-      {/*        className={*/}
-      {/*          "flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"*/}
-      {/*        }*/}
-      {/*      >*/}
-      {/*        <h3 className="text-2xl font-bold">{facility.name} →</h3>*/}
-      {/*        <div className="text-lg">*/}
-      {/*          Just the basics - Everything you need to know to set up your*/}
-      {/*          database and authentication.*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*    ))}*/}
-      {/*    /!*<Link*!/*/}
-      {/*    /!*  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"*!/*/}
-      {/*    /!*  href="https://create.t3.gg/en/usage/first-steps"*!/*/}
-      {/*    /!*  target="_blank"*!/*/}
-      {/*    /!*>*!/*/}
-      {/*    /!*  <h3 className="text-2xl font-bold">First Steps →</h3>*!/*/}
-      {/*    /!*  <div className="text-lg">*!/*/}
-      {/*    /!*    Just the basics - Everything you need to know to set up your*!/*/}
-      {/*    /!*    database and authentication.*!/*/}
-      {/*    /!*  </div>*!/*/}
-      {/*    /!*</Link>*!/*/}
-      {/*    /!*<Link*!/*/}
-      {/*    /!*  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"*!/*/}
-      {/*    /!*  href="https://create.t3.gg/en/introduction"*!/*/}
-      {/*    /!*  target="_blank"*!/*/}
-      {/*    /!*>*!/*/}
-      {/*    /!*  <h3 className="text-2xl font-bold">Documentation →</h3>*!/*/}
-      {/*    /!*  <div className="text-lg">*!/*/}
-      {/*    /!*    Learn more about Create T3 App, the libraries it uses, and how to*!/*/}
-      {/*    /!*    deploy it.*!/*/}
-      {/*    /!*  </div>*!/*/}
-      {/*    /!*</Link>*!/*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </main>
   );
 }
