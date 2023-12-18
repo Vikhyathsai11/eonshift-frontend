bea8024e6b37a98b1f6b5d0810f184512ea9a446
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2023-12-18 19:46:26 +0530

    feat: Updated the device control page.

--- src/app/(with-dashboard-layout)/devices/control/page.tsx
+++ src/app/(with-dashboard-layout)/devices/control/page.tsx
@@ -1,22 +1,97 @@
 "use client";
 
-import { Text } from "@tremor/react";
+import * as React from "react";
+import { useMemo, useState } from "react";
+
+import { Card, Metric, Text } from "@tremor/react";
+import axios from "axios";
+import { collection, orderBy, query, type Query } from "firebase/firestore";
+import { useSelector } from "react-redux";
+import { useFirestoreCollectionData } from "reactfire";
 
 import PageContainer from "~/shared/custom/page-container";
 import PageHeading from "~/shared/custom/page-heading";
+import { Button } from "~/shared/shadcn/ui/button";
 import { Separator } from "~/shared/shadcn/ui/separator";
 
-const ControlPage = () => {
+import { db } from "~/lib/firebase";
+import { Icons } from "~/lib/icons";
+import { type RootState } from "~/redux/store";
+import { type DeviceDocument } from "~/types";
+
+const ControlDevices = () => {
+  const { id: facilityId } = useSelector((state: RootState) => state.facility);
+
+  const [loadingDeviceId, setLoadingDeviceId] = useState<string | null>(null);
+
+  const devicesQuery = useMemo(
+    () =>
+      query(
+        collection(db, "facilities", facilityId, "devices"),
+        orderBy("created_at", "desc"),
+      ) as Query<DeviceDocument>,
+    [facilityId],
+  );
+
+  const { data: devices, status } = useFirestoreCollectionData(devicesQuery, {
+    initialData: [],
+    idField: "id",
+  });
+
+  const handleDeviceToggle = async (deviceId: string, status: string) => {
+    setLoadingDeviceId(deviceId);
+    await axios({
+      method: "post",
+      url: "/api/publish",
+      data: {
+        status,
+        deviceId,
+        facilityId,
+      },
+    });
+    setLoadingDeviceId(null);
+  };
+
   return (
     <PageContainer>
       <PageHeading
-        mainTitle={"Control"}
-        subTitle={"Review and Manage Control Resources."}
+        mainTitle={"Control Device"}
+        subTitle={"Review and control all the device resources."}
       />
       <Separator />
-      <Text>Sample</Text>
+      {status === "loading" && <>Loading Devices</>}
+      {status === "success" && devices && (
+        <div className={"grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-2"}>
+          {devices?.map((device) => {
+            return (
+              <Card
+                decoration={"top"}
+                decorationColor={device?.status == "active" ? "green" : "rose"}
+              >
+                <Text>{device.id}</Text>
+                <Metric>{device.energy_usage} kWh</Metric>
+                <Button
+                  size={"sm"}
+                  onClick={() =>
+                    handleDeviceToggle(
+                      device.id,
+                      device.status === "active" ? "inactive" : "active",
+                    )
+                  }
+                  disabled={loadingDeviceId === device.id}
+                >
+                  {loadingDeviceId === device.id && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  {device.status == "active" ? "Turn Off" : "Turn On"}
+                </Button>
+              </Card>
+            );
+          })}
+        </div>
+      )}
     </PageContainer>
   );
 };
 
-export default ControlPage;
+export default ControlDevices;
