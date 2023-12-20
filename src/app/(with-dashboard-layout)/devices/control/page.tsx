1d651dfb6a595a2d737634dcb82e0ccfa177c869
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2023-12-21 03:28:49 +0530

    fix: Fixed the issues with promise.

--- src/app/(with-dashboard-layout)/devices/control/page.tsx
+++ src/app/(with-dashboard-layout)/devices/control/page.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import { useMemo, useState } from "react";
-import { useRouter } from "next/navigation";
 
 import { Card, Metric, Text } from "@tremor/react";
 import axios from "axios";
@@ -22,7 +21,6 @@ import { type DeviceDocument } from "~/types";
 
 const ControlDevices = () => {
   const { id: facilityId } = useSelector((state: RootState) => state.facility);
-  const router = useRouter();
 
   const [loadingDeviceId, setLoadingDeviceId] = useState<string | null>(null);
 
@@ -55,8 +53,8 @@ const ControlDevices = () => {
   };
 
   const toggleAllDevices = (toggleStatus: string) => {
-    devices.forEach((device) => {
-      handleDeviceToggle(device.id, toggleStatus);
+    devices.map(async (device) => {
+      await handleDeviceToggle(device.id, toggleStatus);
     });
   };
 
