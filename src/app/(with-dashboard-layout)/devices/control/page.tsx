7ea318f036caebf344ca78a4505dc793a1a88e6d
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2023-12-23 11:41:56 +0530

    feat: Added the switch in the system.

--- src/app/(with-dashboard-layout)/devices/control/page.tsx
+++ src/app/(with-dashboard-layout)/devices/control/page.tsx
@@ -4,7 +4,8 @@ import * as React from "react";
 import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { Card, Metric, Text } from "@tremor/react";
+import { type ColumnDef } from "@tanstack/react-table";
+import { Card, Metric, Switch, Text } from "@tremor/react";
 import axios from "axios";
 import {
   collection,
@@ -21,19 +22,189 @@ import { z } from "zod";
 
 import PageContainer from "~/shared/custom/page-container";
 import PageHeading from "~/shared/custom/page-heading";
+import { Badge } from "~/shared/shadcn/ui/badge";
+import { Checkbox } from "~/shared/shadcn/ui/checkbox";
 import { Separator } from "~/shared/shadcn/ui/separator";
 
 import { columns } from "~/app/(with-dashboard-layout)/devices/control/components/datatable/columns";
 import { DataTable } from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data-table";
-import { deviceSchema } from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data/schema";
+import { DataTableColumnHeader } from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data-table-column-header";
+import { DataTableRowActions } from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data-table-row-actions";
+import {
+  labels,
+  priorities,
+  statuses,
+} from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data/data";
+import {
+  deviceSchema,
+  type Task,
+} from "~/app/(with-dashboard-layout)/devices/control/components/datatable/data/schema";
 
 import { db } from "~/lib/firebase";
 import { Icons } from "~/lib/icons";
+import { cn } from "~/lib/utils";
 import { type RootState } from "~/redux/store";
 import { type DeviceDocument } from "~/types";
 
 const ControlDevices = () => {
   const { id: facilityId } = useSelector((state: RootState) => state.facility);
+
+  const columns: ColumnDef<Task>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+          className="translate-y-[2px]"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+          className="translate-y-[2px]"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "id",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Device Id" />
+      ),
+      cell: ({
+        row: {
+          original: { id },
+        },
+      }) => (
+        <div className="w-[100px]">
+          <p>D-{id.substring(0, 5)}</p>
+        </div>
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Name" />
+      ),
+      cell: ({ row }) => {
+        const label = labels.find(
+          (label) => label.value === row.original.type,
+        ) ?? {
+          value: "other",
+          label: "Other",
+        };
+
+        return (
+          <div className="flex space-x-2">
+            {label && <Badge variant="outline">{label.label}</Badge>}
+            <span className="max-w-[500px] truncate font-medium">
+              {row.getValue("name")}
+            </span>
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "status",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Status" />
+      ),
+      cell: ({ row }) => {
+        const status = statuses.find(
+          (status) => status.value === row.getValue("status"),
+        );
+
+        if (!status) {
+          return null;
+        }
+
+        return (
+          <div className="flex w-[100px] items-center">
+            {status.icon && (
+              <status.icon
+                className={cn(
+                  "mr-2 h-4 w-4 text-muted-foreground",
+                  status.value === "active"
+                    ? "text-emerald-500"
+                    : "text-rose-500",
+                )}
+              />
+            )}
+            <span>{status.label}</span>
+          </div>
+        );
+      },
+      filterFn: (row, id, value) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
+        return value.includes(row.getValue(id));
+      },
+    },
+    {
+      accessorKey: "priority",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Priority" />
+      ),
+      cell: ({ row }) => {
+        const priority = priorities.find(
+          (priority) => priority.value === row.getValue("priority"),
+        );
+
+        if (!priority) {
+          return null;
+        }
+
+        return (
+          <div className="flex items-center">
+            {priority.icon && (
+              <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+            )}
+            <span>{priority.label}</span>
+          </div>
+        );
+      },
+      filterFn: (row, id, value) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
+        return value.includes(row.getValue(id));
+      },
+    },
+    {
+      id: "sample",
+      cell: ({ row }) => {
+        return (
+          <div
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
+            <Switch
+              name={"Toggle Device"}
+              checked={row.original.status === "active"}
+              onChange={async () => {
+                await handleDeviceToggle(
+                  row.original.id,
+                  row.original.status === "active" ? "inactive" : "active",
+                );
+              }}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => <DataTableRowActions row={row} />,
+    },
+  ];
 
   const router = useRouter();
   const [loadingDeviceId, setLoadingDeviceId] = useState<string | null>(null);
