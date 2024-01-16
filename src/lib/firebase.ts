0bd97fbca784a40ab812aac826b2d260b61038d3
Vikhyathsai11 <vikhyathsai11@gmail.com>
2024-01-16 17:59:13 +0530

    firebase changed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHS3aNAwPSjiOX2UE2lwLdgy7jGpeVJ-E",
-  authDomain: "enimo-new.firebaseapp.com",
-  projectId: "enimo-new",
-  storageBucket: "enimo-new.appspot.com",
-  messagingSenderId: "94435480428",
-  appId: "1:94435480428:web:0e00d71643c62e5109d949",
-  measurementId: "G-1KJHV8VF2W",
+
 };
 
 // Initialize Firebase
