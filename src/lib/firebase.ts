70e85bc1059cc9c0af19759dd5725f209bc0aad7
Vikhyathsai11 <vikhyathsai11@gmail.com>
2024-02-04 13:41:55 +0530

    updates api

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyB_KCYiZkP7hxHOZJxZ906TAwn47v8yWqs",
+  authDomain: "eonshift-562d1.firebaseapp.com",
+  projectId: "eonshift-562d1",
+  storageBucket: "eonshift-562d1.appspot.com",
+  messagingSenderId: "511989834545",
+  appId: "1:511989834545:web:b4e66e3e9f380f77344610",
+  measurementId: "G-CE280PTTNQ"
 };
 
 // Initialize Firebase
